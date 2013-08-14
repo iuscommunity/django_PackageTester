e93c8f6d43b90a6f1bd9f5ccb60f200634d13340
Arnoud Vermeer <a.vermeer@freshway.biz>
2013-08-14 15:40:47 +0200

    Update settings-example.py
    Fixing the routing

--- settings-example.py
+++ settings-example.py
@@ -113,7 +113,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'django_ius_package_tester.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
